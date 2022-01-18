383517644b7a94e329d9f86f3a3341370f21d5e9
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-18 15:30:21 -0600

    Added the code to run a Query with Relay Modern

--- @dum/client/src/pages/login.tsx
+++ @dum/client/src/pages/login.tsx
@@ -1,14 +1,38 @@
 import { Text } from "native-base";
 import * as React from "react";
+import { usePreloadedQuery } from "react-relay";
+import type { RelayProps } from "relay-nextjs";
+import { withRelay } from "relay-nextjs";
 
 import Layout from "../components/Layout";
+import type { LoginPageQuery as LoginPageQueryTypes } from "../graphql/__generated__/LoginPageQuery.graphql";
+import LoginPageQuery from "../graphql/LoginPageQuery";
+import { getClientEnvironment } from "../lib/client";
 
-function LoginPage() {
+function LoginPage({ preloadedQuery }: RelayProps<{}, LoginPageQueryTypes>) {
+  const loginPageQuery = usePreloadedQuery<LoginPageQueryTypes>(
+    LoginPageQuery,
+    preloadedQuery
+  );
+
   return (
-    <Layout>
+    <Layout currentUser={loginPageQuery.currentUser}>
       <Text>Login</Text>
     </Layout>
   );
 }
 
-export default LoginPage;
+export default withRelay(LoginPage, LoginPageQuery, {
+  createClientEnvironment: () => getClientEnvironment(),
+  createServerEnvironment: async (_ctx, { cookies }) => {
+    const { createServerEnvironment } = await import("../lib/server/server");
+
+    return createServerEnvironment(cookies);
+  },
+  fallback: () => <Text>Cargando tus pinches datos prro</Text>,
+  serverSideProps: async (ctx) => {
+    const cookies = ctx.req.headers.cookie;
+
+    return { cookies };
+  },
+});
