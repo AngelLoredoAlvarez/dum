b46f5d81ffe5bb781d5fb3b1961fd1b864ef397f
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-20 10:44:49 -0600

    Added the reponsive margin props to all the components

--- @dum/client/src/components/Post.tsx
+++ @dum/client/src/components/Post.tsx
@@ -16,50 +16,116 @@ function Post(props: PostProps) {
     <Box flex={1}>
       <Image
         alt={post.headline}
-        h={"50%"}
+        h={"45%"}
+        mb={3}
+        ml={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
+        }}
+        mr={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
+        }}
+        mt={3}
+        rounded={"lg"}
         source={{
           uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
         }}
-        w={"100%"}
       />
       <Text
         bold
-        borderWidth={1}
         fontSize={{
           base: "2xl",
           sm: "2xl",
-          md: "lg",
-          lg: "xl",
-          xl: "xl",
-          "2xl": "7xl",
+          md: "2xl",
+          lg: "4xl",
+          xl: "4xl",
+          "2xl": "6xl",
+        }}
+        mb={3}
+        ml={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
+        }}
+        mr={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
         }}
         textAlign={"center"}
       >
         {post.headline}
       </Text>
       <Text
-        italic
         fontSize={{
-          base: "2xl",
-          sm: "2xl",
+          base: "md",
+          sm: "md",
           md: "lg",
           lg: "xl",
           xl: "xl",
           "2xl": "2xl",
+        }}
+        italic
+        mb={3}
+        ml={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
+        }}
+        mr={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
         }}
         textAlign={"center"}
       >
         {post.fullDate} ({post.age})
       </Text>
       <Text
-        borderWidth={1}
         fontSize={{
-          base: "2xl",
-          sm: "2xl",
+          base: "md",
+          sm: "md",
           md: "lg",
           lg: "xl",
           xl: "xl",
-          "2xl": "4xl",
+          "2xl": "2xl",
+        }}
+        ml={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
+        }}
+        mr={{
+          base: 3,
+          sm: 3,
+          md: 20,
+          lg: 32,
+          xl: 48,
+          "2xl": 56,
         }}
         textAlign={"justify"}
       >
