2840163fe2bfa5ff763ccb1976a74e14427a30c3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-28 09:27:04 -0600

    Changed the 'id' prop to 'rowId' according the new GraqhQL Schema, also, added the SubDepartmentsFragment_subDepartments Fragment

--- @dum/client/src/graphql/Queries/MainDepartmentPageQuery.ts
+++ @dum/client/src/graphql/Queries/MainDepartmentPageQuery.ts
@@ -1,10 +1,16 @@
 import { graphql } from "react-relay/hooks";
 
 export default graphql`
-  query MainDepartmentPageQuery {
+  query MainDepartmentPageQuery($rowId: UUID!) {
     currentUser {
       ...CurrentUserFragment_user
     }
     ...MainDepartmentsFragment_mainDepartments
+    mainDepartment(rowId: $rowId) {
+      id
+      rowId
+      mainDepartment
+      ...SubDepartmentsFragment_subDepartments
+    }
   }
 `;
