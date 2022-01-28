3ad2e11228b651e6880cc3a859409d85ec6ae3b1
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-28 08:59:11 -0600

    Created the SubDepartmentsFragment.ts Fragment wich will query a connection of all the Sub - Departments in a specific Main Department

---
+++ @dum/client/src/graphql/Fragments/SubDepartmentsFragment.ts
@@ -0,0 +1,19 @@
+import { graphql } from "react-relay/hooks";
+
+export default graphql`
+  fragment SubDepartmentsFragment_subDepartments on MainDepartment
+  @argumentDefinitions(
+    first: { type: "Int!", defaultValue: 10 }
+    after: { type: "Cursor" }
+  )
+  @refetchable(queryName: "SubDepartments") {
+    subDepartmentsByDepartmentId(first: $first, after: $after)
+      @connection(key: "SubDepartmentsFragment_subDepartmentsByDepartmentId") {
+      edges {
+        node {
+          ...SubDepartmentFragment_subDepartment
+        }
+      }
+    }
+  }
+`;
