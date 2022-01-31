8ddd92877fc2d879dc7d72fa662914f9d51119cf
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-31 08:11:33 -0600

    Added the id and rowId fields

--- @dum/client/src/graphql/Queries/StorePageQuery.ts
+++ @dum/client/src/graphql/Queries/StorePageQuery.ts
@@ -3,6 +3,8 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query StorePageQuery {
     currentUser {
+      id
+      rowId
       ...CurrentUserFragment_user
     }
     ...MainDepartmentsFragment_mainDepartments
