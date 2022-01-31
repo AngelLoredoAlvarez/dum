ea1c12bba32ff5dc675db2a7cea9257f4b90c15d
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-01-31 08:09:51 -0600

    Added the id and rowId field

--- @dum/client/src/graphql/Queries/LoginPageQuery.ts
+++ @dum/client/src/graphql/Queries/LoginPageQuery.ts
@@ -3,6 +3,8 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query LoginPageQuery {
     currentUser {
+      id
+      rowId
       ...CurrentUserFragment_user
     }
   }
