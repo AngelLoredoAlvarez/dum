e90ccf97a414048172dbac95be5d2e7b221ffbbd
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-03 10:55:30 -0600

    Added the <PostsList /> component that will Query and Render all the Posts

---
+++ @dum/client/src/components/PostsList.tsx
@@ -0,0 +1,25 @@
+import { FlatList } from "native-base";
+import * as React from "react";
+import { usePaginationFragment } from "react-relay/hooks";
+
+import { PostsFragment_posts$key } from "../graphql/Fragments/__generated__/PostsFragment_posts.graphql";
+import PostsFragment from "../graphql/Fragments/PostsFragment";
+import PostListItem from "./PostListItem";
+
+interface PostsListProps {
+  posts: PostsFragment_posts$key;
+}
+
+function PostsList(props: PostsListProps) {
+  const { data } = usePaginationFragment(PostsFragment, props.posts);
+
+  return (
+    <FlatList
+      data={data.posts.edges}
+      keyExtractor={(item) => item.node.id}
+      renderItem={({ item }) => <PostListItem post={item.node} />}
+    />
+  );
+}
+
+export default PostsList;
