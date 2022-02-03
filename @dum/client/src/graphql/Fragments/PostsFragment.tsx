50307f4fc33f70f7ca51fad43de472b4dea4d959
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-03 10:54:44 -0600

    Added the Fragment that will Query the Posts

---
+++ @dum/client/src/graphql/Fragments/PostsFragment.tsx
@@ -0,0 +1,20 @@
+import { graphql } from "react-relay/hooks";
+
+export default graphql`
+  fragment PostsFragment_posts on Query
+  @argumentDefinitions(
+    first: { type: "Int!", defaultValue: 10 }
+    after: { type: "Cursor" }
+  )
+  @refetchable(queryName: "Posts") {
+    posts(first: $first, after: $after)
+      @connection(key: "PostsFragment_posts") {
+      edges {
+        node {
+          id
+          ...PostFragment_post
+        }
+      }
+    }
+  }
+`;
