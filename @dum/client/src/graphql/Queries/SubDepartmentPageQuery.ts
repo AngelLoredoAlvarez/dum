fa5492b057cd3d146e1d7ddcdcd65e07c0272477
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-07 11:49:45 -0600

    Created the SubDepartmentPageQuery wich will query all the information related to a specific Sub - Department

---
+++ @dum/client/src/graphql/Queries/SubDepartmentPageQuery.ts
@@ -0,0 +1,15 @@
+import { graphql } from "react-relay/hooks";
+
+export default graphql`
+  query SubDepartmentPageQuery($subDepartment: String!) {
+    currentUser {
+      ...CurrentUserFragment_user
+    }
+    ...MainDepartmentsFragment_mainDepartments
+    subDepartmentByName(arg0: $subDepartment) {
+      id
+      rowId
+      subDepartment
+    }
+  }
+`;
