032d466380e9fbef93b84c42cebbddfc34a866e3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-08 15:42:31 -0600

    Added different props to make a better list

--- @dum/client/src/components/SubDepartmentProductsList.tsx
+++ @dum/client/src/components/SubDepartmentProductsList.tsx
@@ -2,12 +2,17 @@ import { FlatList } from "native-base";
 import * as React from "react";
 import { usePaginationFragment } from "react-relay/hooks";
 
+import { MainDepartmentsFragment_mainDepartments$key } from "../graphql/Fragments/__generated__/MainDepartmentsFragment_mainDepartments.graphql";
+import { SubDepartmentFragment_subDepartment$key } from "../graphql/Fragments/__generated__/SubDepartmentFragment_subDepartment.graphql";
 import { SubDepartmentProductsFragment_products$key } from "../graphql/Fragments/__generated__/SubDepartmentProductsFragment_products.graphql";
 import SubDepartmentProductsFragment from "../graphql/Fragments/SubDepartmentProductsFragment";
+import SubDepartmentProductsListHeader from "./SubDepartmentProductsListHeader";
 import SubDepartmentProductsListItem from "./SubDepartmentProductsListItem";
 
 interface SubDepartmentProductsListProps {
+  mainDepartments: MainDepartmentsFragment_mainDepartments$key;
   products: SubDepartmentProductsFragment_products$key;
+  subDepartment: SubDepartmentFragment_subDepartment$key;
 }
 
 function SubDepartmentProductsList(props: SubDepartmentProductsListProps) {
@@ -18,12 +23,15 @@ function SubDepartmentProductsList(props: SubDepartmentProductsListProps) {
 
   return (
     <FlatList
-      alignContent={"center"}
-      alignSelf={"center"}
-      borderWidth={1}
       data={data.products.edges}
-      flexWrap={1}
+      flex={1}
       keyExtractor={(item) => item.node.id}
+      ListHeaderComponent={
+        <SubDepartmentProductsListHeader
+          mainDepartments={props.mainDepartments}
+          subDepartment={props.subDepartment}
+        />
+      }
       numColumns={6}
       onEndReached={() => {
         loadNext(6);
@@ -32,6 +40,7 @@ function SubDepartmentProductsList(props: SubDepartmentProductsListProps) {
       renderItem={({ item }) => (
         <SubDepartmentProductsListItem product={item.node} />
       )}
+      w={"100%"}
     />
   );
 }
