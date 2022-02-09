5278d06ad52c136a7d5f6189259b4f895df6f45f
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-09 14:32:06 -0600

    Added a <Pressable /> component to apply some efects when the user is hovering a product

--- @dum/client/src/components/SubDepartmentProductsListItem.tsx
+++ @dum/client/src/components/SubDepartmentProductsListItem.tsx
@@ -1,4 +1,4 @@
-import { Heading, Image, Text, VStack } from "native-base";
+import { Heading, Image, Pressable, Text, VStack } from "native-base";
 import * as React from "react";
 import { useFragment } from "react-relay/hooks";
 
@@ -15,62 +15,73 @@ function SubDepartmentProductsListItem(
   const product = useFragment(ProductFragment, props.product);
 
   return (
-    <VStack
-      _dark={{ backgroundColor: "gray.700" }}
-      _light={{ backgroundColor: "coolGray.50" }}
-      alignContent={"center"}
-      alignItems={"center"}
-      alignSelf={"center"}
-      h={"450"}
-      mb={5}
-      ml={{
-        base: 4,
-        sm: 4,
-        md: 4,
-        lg: 4,
-        xl: 4,
-        "2xl": 44,
-      }}
-      overflow="hidden"
-      rounded="lg"
-      shadow="9"
-      space={2}
-      w={{
-        sm: "210",
-        md: "220",
-        lg: "230",
-        xl: "240",
-        "2xl": "250",
-      }}
-    >
-      <Image
-        alt={product.description}
-        h={"70%"}
-        source={{
-          uri: `${product.pictureUrl}`,
-        }}
-        w={"100%"}
-      />
-      <Heading
-        fontSize={{
-          base: "sm",
-          sm: "sm",
-          md: "sm",
-          lg: "md",
-          xl: "lg",
-          "2xl": "xl",
-        }}
-        textAlign={"center"}
-      >
-        {product.brand.brand}
-      </Heading>
-      <Text fontSize={"sm"} textAlign={"center"}>
-        {product.description}
-      </Text>
-      <Text bold fontSize={"sm"} textAlign={"center"}>
-        {product.price}
-      </Text>
-    </VStack>
+    <Pressable>
+      {({ isHovered }) => (
+        <VStack
+          _dark={{ backgroundColor: "gray.700" }}
+          _light={{ backgroundColor: "coolGray.50" }}
+          alignContent={"center"}
+          alignItems={"center"}
+          alignSelf={"center"}
+          h={"450"}
+          mb={5}
+          ml={{
+            base: 4,
+            sm: 4,
+            md: 4,
+            lg: 4,
+            xl: 4,
+            "2xl": 44,
+          }}
+          overflow="hidden"
+          rounded="lg"
+          shadow="9"
+          style={{
+            transform: [
+              {
+                scale: isHovered ? 1 : 0.96,
+              },
+            ],
+          }}
+          space={2}
+          w={{
+            sm: "210",
+            md: "220",
+            lg: "230",
+            xl: "240",
+            "2xl": "250",
+          }}
+        >
+          <Image
+            alt={product.description}
+            h={"70%"}
+            source={{
+              uri: `${product.pictureUrl}`,
+            }}
+            w={"100%"}
+          />
+          <Heading
+            fontSize={{
+              base: "sm",
+              sm: "sm",
+              md: "sm",
+              lg: "md",
+              xl: "lg",
+              "2xl": "xl",
+            }}
+            textAlign={"center"}
+          >
+            {product.brand.brand}
+          </Heading>
+          <Text fontSize={"sm"} textAlign={"center"}>
+            {product.description}
+          </Text>
+          <Text bold fontSize={"sm"} textAlign={"center"}>
+            {product.price}
+          </Text>
+        </VStack>
+      )}
+    </Pressable>
   );
 }
 
