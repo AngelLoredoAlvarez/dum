778df1d5256afe75ae55473d6c83d37c6cee4535
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-14 09:44:32 -0600

    Added a style when the uses is hovering an image in the list

--- @dum/client/src/components/ProductPicturesViewer.tsx
+++ @dum/client/src/components/ProductPicturesViewer.tsx
@@ -1,5 +1,6 @@
 import { Box, HStack, Image, Pressable, VStack } from "native-base";
 import * as React from "react";
+
 interface ProductPicturesViewerProps {
   productPictures: Array<any>;
 }
@@ -10,12 +11,19 @@ function ProductPicturesViewer(props: ProductPicturesViewerProps) {
       <VStack borderWidth={1} h={"100%"} w={"10%"}>
         {props.productPictures.map(({ node }) => (
           <Pressable h={"15%"} key={node.id} w={"100%"}>
-            {() => (
+            {({ isHovered }) => (
               <Image
                 alt={node.id}
                 h={"100%"}
                 source={{
                   uri: `${node.pictureUrl}`,
+                }}
+                style={{
+                  transform: [
+                    {
+                      scale: isHovered ? 1 : 0.96,
+                    },
+                  ],
                 }}
                 w={"100%"}
               />
