1c165a1d331d454bb058ab25b6a4c016896232db
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-14 07:59:37 -0600

    Removed the product Relay Fragment Reference, it will be an array from now

--- @dum/client/src/components/Product.tsx
+++ @dum/client/src/components/Product.tsx
@@ -12,10 +12,11 @@ interface ProductProps {
 
 function Product(props: ProductProps) {
   const product = useFragment(ProductFragment, props.product);
+  const productPictures = [...product.productPictures.edges];
 
   return (
     <HStack flex={1} flexGrow={1} mb={10} ml={48} mr={48} mt={7}>
-      <ProductPicturesViewer productPictures={product} />
+      <ProductPicturesViewer productPictures={productPictures} />
       <VStack
         alignContent={"center"}
         alignItems={"center"}
