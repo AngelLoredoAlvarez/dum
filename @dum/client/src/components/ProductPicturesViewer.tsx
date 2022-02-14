79a41bd7ef7e52e4002fdd2349b4a68511c67a4b
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-14 08:01:12 -0600

    Removed all the Relay Code, for this specific component, we only need to pass an array of the items, thats because we don't need any specific feature like pagination

--- @dum/client/src/components/ProductPicturesViewer.tsx
+++ @dum/client/src/components/ProductPicturesViewer.tsx
@@ -1,37 +1,21 @@
 import { Box, HStack, Image, Pressable, VStack } from "native-base";
 import * as React from "react";
-import { useFragment } from "react-relay/hooks";
-
-import { ProductPicturesFragment_productPictures$key } from "../graphql/Fragments/__generated__/ProductPicturesFragment_productPictures.graphql";
-import ProductPicturesFragment from "../graphql/Fragments/ProductPicturesFragment";
-
 interface ProductPicturesViewerProps {
-  productPictures: ProductPicturesFragment_productPictures$key;
+  productPictures: Array<any>;
 }
 
 function ProductPicturesViewer(props: ProductPicturesViewerProps) {
-  const productPictures = useFragment(
-    ProductPicturesFragment,
-    props.productPictures
-  );
-
   return (
     <HStack flex={1} flexGrow={1}>
-      <VStack h={"100%"} w={"10%"}>
-        {productPictures.productPictures.edges.map(({ node }) => (
+      <VStack borderWidth={1} h={"100%"} w={"10%"}>
+        {props.productPictures.map(({ node }) => (
           <Pressable h={"15%"} key={node.id} w={"100%"}>
-            {({ isHovered }) => (
+            {() => (
               <Image
+                alt={node.id}
                 h={"100%"}
                 source={{
                   uri: `${node.pictureUrl}`,
-                }}
-                style={{
-                  transform: [
-                    {
-                      scale: isHovered ? 1 : 0.96,
-                    },
-                  ],
                 }}
                 w={"100%"}
               />
@@ -39,7 +23,7 @@ function ProductPicturesViewer(props: ProductPicturesViewerProps) {
           </Pressable>
         ))}
       </VStack>
-      <Box borderWidth={1} flex={1} flexGrow={1} />
+      <Box borderWidth={1} flex={1} flexGrow={1}></Box>
     </HStack>
   );
 }
