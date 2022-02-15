f73e862a40771ca4b15879b6103c36e497f4d407
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-15 08:26:24 -0600

    Added the Relay Modern Code to refetch the Picture when the user clicks on a picture

--- @dum/client/src/components/ProductPicturesViewer.tsx
+++ @dum/client/src/components/ProductPicturesViewer.tsx
@@ -1,16 +1,31 @@
 import { Box, HStack, Image, Pressable, VStack } from "native-base";
 import * as React from "react";
+import { useRefetchableFragment } from "react-relay/hooks";
+
+import { ProductPictureFragment_productPicture$key } from "../graphql/Fragments/__generated__/ProductPictureFragment_productPicture.graphql";
+import ProductPictureFragment from "../graphql/Fragments/ProductPictureFragment";
 
 interface ProductPicturesViewerProps {
+  productPicture: ProductPictureFragment_productPicture$key;
   productPictures: Array<any>;
 }
 
 function ProductPicturesViewer(props: ProductPicturesViewerProps) {
+  const [data, refetch] = useRefetchableFragment(
+    ProductPictureFragment,
+    props.productPicture
+  );
+
   return (
     <HStack flex={1} flexGrow={1}>
       <VStack borderWidth={1} h={"100%"} w={"10%"}>
         {props.productPictures.map(({ node }) => (
-          <Pressable h={"15%"} key={node.id} w={"100%"}>
+          <Pressable
+            h={"15%"}
+            key={node.id}
+            onPress={() => refetch({ pictureId: node.rowId })}
+            w={"100%"}
+          >
             {({ isHovered }) => (
               <Image
                 alt={node.id}
@@ -31,7 +46,16 @@ function ProductPicturesViewer(props: ProductPicturesViewerProps) {
           </Pressable>
         ))}
       </VStack>
-      <Box borderWidth={1} flex={1} flexGrow={1}></Box>
+      <Box borderWidth={1} flex={1} flexGrow={1}>
+        <Image
+          alt={data.pictureById.id}
+          h={"100%"}
+          source={{
+            uri: `${data.pictureById.pictureUrl}`,
+          }}
+          w={"100%"}
+        />
+      </Box>
     </HStack>
   );
 }
