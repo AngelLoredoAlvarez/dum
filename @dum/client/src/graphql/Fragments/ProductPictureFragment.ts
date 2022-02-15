fd5b42264b82387ca3cf1bbdddffbbef527fd5e3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-15 08:29:57 -0600

    Fixed the refetchable fragment that fetch the picture according it's ID

--- @dum/client/src/graphql/Fragments/ProductPictureFragment.ts
+++ @dum/client/src/graphql/Fragments/ProductPictureFragment.ts
@@ -1,15 +1,10 @@
 import { graphql } from "react-relay/hooks";
 
 export default graphql`
-  fragment ProductPictureFragment_productPicture on Query
-  @argumentDefinitions(
-    pictureId: {
-      type: "UUID"
-      defaultValue: "0b7e8193-a31d-4ccf-8986-6d2e156edb99"
-    }
-  )
+  fragment ProductPictureFragment_productPicture on Product
+  @argumentDefinitions(pictureId: { type: "UUID" })
   @refetchable(queryName: "ProductPictureFragment") {
-    productPicture(rowId: $pictureId) {
+    pictureById(pictureId: $pictureId) {
       id
       rowId
       pictureUrl
