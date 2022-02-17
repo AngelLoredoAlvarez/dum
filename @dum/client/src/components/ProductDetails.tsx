b9567a09086064ca7ec86070b7ef0b8bcf3b5867
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-17 13:03:26 -0600

    Added the <ProductDetails /> component that will display the details of a Product

---
+++ @dum/client/src/components/ProductDetails.tsx
@@ -0,0 +1,53 @@
+import { Button, Text, VStack } from "native-base";
+import * as React from "react";
+import { useFragment } from "react-relay/hooks";
+
+import type { ProductFragment_product$key } from "../graphql/Fragments/__generated__/ProductFragment_product.graphql";
+import ProductFragment from "../graphql/Fragments/ProductFragment";
+
+function ProductDetails(props: { product: ProductFragment_product$key }) {
+  const product = useFragment<ProductFragment_product$key>(
+    ProductFragment,
+    props.product
+  );
+
+  return (
+    <VStack
+      alignContent={"center"}
+      alignItems={"center"}
+      borderWidth={1}
+      mr={{
+        lg: 10,
+        xl: 20,
+        "2xl": 48,
+      }}
+      space={5}
+      h={{
+        base: null,
+        sm: null,
+        md: null,
+        lg: "100%",
+        xl: "100%",
+        "2xl": "100%",
+      }}
+      w={{
+        base: "95%",
+        sm: "95%",
+        md: "97%",
+        lg: null,
+        xl: null,
+        "2xl": null,
+      }}
+    >
+      <Text textAlign={"center"}>{product.brand.brand}</Text>
+      <Text bold textAlign={"center"}>
+        {product.description}
+      </Text>
+      <Text textAlign={"center"}>{product.price}</Text>
+      <Text textAlign={"center"}>{product.stock}</Text>
+      <Button>Agregar al Carrito</Button>
+    </VStack>
+  );
+}
+
+export default ProductDetails;
