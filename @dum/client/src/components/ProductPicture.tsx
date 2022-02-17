f1db97f127a263ec7417f163a6c68987c130a2d3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-17 13:04:23 -0600

    Added the <ProductPicture /> component that will show a selected product pictures in the Product Pictures List

---
+++ @dum/client/src/components/ProductPicture.tsx
@@ -0,0 +1,54 @@
+import { Box, Image } from "native-base";
+import * as React from "react";
+import { useRefetchableFragment } from "react-relay/hooks";
+
+import { ProductPictureFragment_productPicture$key } from "../graphql/Fragments/__generated__/ProductPictureFragment_productPicture.graphql";
+import ProductPictureFragment from "../graphql/Fragments/ProductPictureFragment";
+
+interface ProductPictureProps {
+  pictureId: any;
+  productPicture: ProductPictureFragment_productPicture$key;
+}
+
+function ProductPicture(props: ProductPictureProps) {
+  const [data, refetch] = useRefetchableFragment(
+    ProductPictureFragment,
+    props.productPicture
+  );
+
+  React.useEffect(() => {
+    refetch({ pictureId: props.pictureId });
+  }, [props.pictureId, refetch]);
+
+  return (
+    <Box
+      borderWidth={1}
+      flex={1}
+      h={{
+        base: "60%",
+        sm: "60%",
+        md: "58%",
+        lg: "100%",
+        xl: "100%",
+        "2xl": "100%",
+      }}
+      w={{
+        base: "95%",
+        sm: "95%",
+        md: "97%",
+        lg: null,
+        xl: null,
+        "2xl": null,
+      }}
+    >
+      <Image
+        alt={data.productPicture.id}
+        h={"100%"}
+        source={{ uri: `${data.productPicture.pictureUrl}` }}
+        w={"100%"}
+      />
+    </Box>
+  );
+}
+
+export default ProductPicture;
