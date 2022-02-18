51ec5ab548b0c30dc3e681e503d729f72bedf150
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-18 14:33:31 -0600

    Removed unnecesary fields in the currentUser query

--- @dum/client/src/graphql/Queries/StorePageQuery.ts
+++ @dum/client/src/graphql/Queries/StorePageQuery.ts
@@ -3,8 +3,6 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query StorePageQuery {
     currentUser {
-      id
-      rowId
       ...CurrentUserFragment_user
     }
     ...MainDepartmentsFragment_mainDepartments
