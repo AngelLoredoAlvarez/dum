c8d8c8fccbb05a4872df12cfc2fca4ec94f8e650
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-22 10:58:40 -0600

    Added the CurrentUserFragment_user fragment in the root Query type

--- @dum/client/src/graphql/Queries/IndexPageQuery.ts
+++ @dum/client/src/graphql/Queries/IndexPageQuery.ts
@@ -2,9 +2,7 @@ import { graphql } from "react-relay/hooks";
 
 export default graphql`
   query IndexPageQuery {
-    currentUser {
-      ...CurrentUserFragment_user
-    }
+    ...CurrentUserFragment_user
     ...PostsFragment_posts
   }
 `;
