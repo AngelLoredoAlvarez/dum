c8e249f0fec68b2bffe9621046328eaf9d5c1ac3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-22 09:31:18 -0600

    Added the currentUser {} field to the BestSellerFragment

--- @dum/client/src/graphql/Fragments/BestSellersFragment.ts
+++ @dum/client/src/graphql/Fragments/BestSellersFragment.ts
@@ -2,6 +2,11 @@ import { graphql } from "react-relay/hooks";
 
 export default graphql`
   fragment BestSellersFragment_bestSellers on Query {
+    currentUser {
+      id
+      rowId
+      fullName
+    }
     products(first: 10) {
       edges {
         node {
