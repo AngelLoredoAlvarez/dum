34c2d0af1cfafe598d12fff8a3e3d7a11938b7ae
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-22 10:59:29 -0600

    Added the CurrentUserFragment_user fragment in the root Query type

--- @dum/client/src/graphql/Queries/PostPageQuery.ts
+++ @dum/client/src/graphql/Queries/PostPageQuery.ts
@@ -2,9 +2,7 @@ import { graphql } from "react-relay/hooks";
 
 export default graphql`
   query PostPageQuery($rowId: UUID!) {
-    currentUser {
-      ...CurrentUserFragment_user
-    }
+    ...CurrentUserFragment_user
     post(rowId: $rowId) {
       ...PostFragment_post
     }
