8b89cc03f2f2460274851b41552444e03b2b2ecf
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-23 12:08:44 -0600

    Removed the currentUser field from the fragment, it won't be neccesary anymore

--- @dum/client/src/graphql/Fragments/BestSellersFragment.ts
+++ @dum/client/src/graphql/Fragments/BestSellersFragment.ts
@@ -2,11 +2,6 @@ import { graphql } from "react-relay/hooks";
 
 export default graphql`
   fragment BestSellersFragment_bestSellers on Query {
-    currentUser {
-      id
-      rowId
-      fullName
-    }
     products(first: 10) {
       edges {
         node {
