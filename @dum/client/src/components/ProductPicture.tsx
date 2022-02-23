742b32a0c88efb440d16af4e5ea8c993000bfdc3
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-23 11:53:10 -0600

    Changed some height and width responsive values, also, removed some unnecesary flex props

--- @dum/client/src/components/ProductPicture.tsx
+++ @dum/client/src/components/ProductPicture.tsx
@@ -22,11 +22,10 @@ function ProductPicture(props: ProductPictureProps) {
 
   return (
     <Box
-      flex={1}
       h={{
-        base: "60%",
-        sm: "60%",
-        md: "58%",
+        base: "45%",
+        sm: "45%",
+        md: "43%",
         lg: "100%",
         xl: "100%",
         "2xl": "100%",
@@ -35,15 +34,16 @@ function ProductPicture(props: ProductPictureProps) {
         base: "95%",
         sm: "95%",
         md: "97%",
-        lg: null,
-        xl: null,
-        "2xl": null,
+        lg: "40%",
+        xl: "40%",
+        "2xl": "40%",
       }}
     >
       <Image
         alt={data.productPicture.id}
-        flex={1}
+        h={"100%"}
         source={{ uri: `${data.productPicture.pictureUrl}` }}
+        w={"100%"}
       />
     </Box>
   );
