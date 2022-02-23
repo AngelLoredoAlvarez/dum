eff5fdca14f0d3a0df2bf650a57a99e8c3e4ee16
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-23 11:24:59 -0600

    Added the currentUser {} field to the query to apply the correct logic when the user tries to add to ther cart without logged in in the app

--- @dum/client/src/graphql/Queries/StorePageQuery.ts
+++ @dum/client/src/graphql/Queries/StorePageQuery.ts
@@ -3,6 +3,11 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query StorePageQuery {
     ...CurrentUserFragment_user
+    currentUser {
+      id
+      rowId
+      fullName
+    }
     ...MainDepartmentsFragment_mainDepartments
     ...BestSellersFragment_bestSellers
   }
