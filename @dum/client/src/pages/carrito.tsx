17920af90035e8d93850400a9c69aee2a892a7dd
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-24 08:35:13 -0600

    Added the ShoppingCart page

---
+++ @dum/client/src/pages/carrito.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "native-base";
+import * as React from "react";
+
+function ShoppingCartPage() {
+  return (
+    <Box>
+      <Text>Shopping Cart Page</Text>
+    </Box>
+  );
+}
+
+export default ShoppingCartPage;
