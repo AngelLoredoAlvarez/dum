bf9354dec895b64dd96c3a35bcc8c7078d7b4d0e
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-24 15:41:10 -0600

    Added the MainDepartmentsFragment_mainDepartments fragment

--- @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
+++ @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
@@ -3,5 +3,6 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query ShoppingListPageQuery {
     ...CurrentUserFragment_user
+    ...MainDepartmentsFragment_mainDepartments
   }
 `;
