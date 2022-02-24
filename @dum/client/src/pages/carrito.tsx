738ca50f25893dfbfe6fd8bf389110a8c5f83f7e
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-24 15:04:09 -0600

    Added all the Relay Modern Code to execute the Query that will retrive all the information of a User Shopping List

--- @dum/client/src/pages/carrito.tsx
+++ @dum/client/src/pages/carrito.tsx
@@ -1,12 +1,45 @@
 import { Box, Text } from "native-base";
 import * as React from "react";
+import { usePreloadedQuery } from "react-relay";
+import type { RelayProps } from "relay-nextjs";
+import { withRelay } from "relay-nextjs";
 
-function ShoppingCartPage() {
+import Layout from "../components/Layout";
+import Loading from "../components/Loading";
+import type { ShoppingListPageQuery as ShoppingListQueryTypes } from "../graphql/Queries/__generated__/ShoppingListPageQuery.graphql";
+import ShoppingListPageQuery from "../graphql/Queries/ShoppingListPageQuery";
+import { getClientEnvironment } from "../lib/client";
+
+function ShoppingListPage({
+  preloadedQuery,
+}: RelayProps<{}, ShoppingListQueryTypes>) {
+  const shoppingListPageQuery = usePreloadedQuery<ShoppingListQueryTypes>(
+    ShoppingListPageQuery,
+    preloadedQuery
+  );
+
+  console.log(shoppingListPageQuery);
+
   return (
-    <Box>
-      <Text>Shopping Cart Page</Text>
-    </Box>
+    <Layout currentUser={shoppingListPageQuery}>
+      <Box>
+        <Text>Shopping Cart Page</Text>
+      </Box>
+    </Layout>
   );
 }
 
-export default ShoppingCartPage;
+export default withRelay(ShoppingListPage, ShoppingListPageQuery, {
+  createClientEnvironment: () => getClientEnvironment(),
+  createServerEnvironment: async (_ctx, { cookies }) => {
+    const { createServerEnvironment } = await import("../lib/server/server");
+
+    return createServerEnvironment(cookies);
+  },
+  fallback: <Loading />,
+  serverSideProps: async (ctx) => {
+    const cookies = ctx.req.headers.cookie;
+
+    return { cookies };
+  },
+});
