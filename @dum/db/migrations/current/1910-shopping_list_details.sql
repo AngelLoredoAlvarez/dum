8296bcd7e97d81ecb8e79b09c5b5006f1a66e75f
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-02-28 12:21:41 -0600

    Added the function that inserts in the dum_public.shopping_lists and dum_public.shopping_list_details tables when the user adds a new product to their shopping list

--- @dum/db/migrations/current/1910-shopping_list_details.sql
+++ @dum/db/migrations/current/1910-shopping_list_details.sql
@@ -31,3 +31,55 @@ create index idx_user_shopping_list_details on dum_public.shopping_list_details(
 create or replace function dum_public.shopping_list_details_cost(shopping_list_detail dum_public.shopping_list_details) returns text as $$
   select cast(shopping_list_detail.unformated_cost as money);
 $$ language sql stable;
+
+/*
+ * Function that inserts into the dum_public.shopping_list and dum_public.shopping_list_details tables
+ */
+create or replace function dum_public.create_shopping_list(product_id uuid, quantity integer) returns dum_public.shopping_list_details as $$
+  declare
+    created_shopping_list dum_public.shopping_lists;
+    calculated_unformated_cost numeric(8,2);
+    added_product dum_public.shopping_list_details;
+  begin
+    -- We calculate the unformated_cost, multiplying the unformated_price by the quantity
+    select unformated_price * $2 from dum_public.products into calculated_unformated_cost;
+
+    -- We check if there's a shopping list already
+    if exists(select 1 from dum_public.shopping_lists where is_sold = false and user_id = dum_public.current_user_id()) then
+      -- If it does, we insert only in the dum_public.shopping_list_details table
+      select * from dum_public.shopping_lists where is_sold = false and user_id = dum_public.current_user_id() into created_shopping_list;
+
+      insert into dum_public.shopping_list_details (
+        quantity,
+        unformated_cost,
+        shopping_list_id
+      ) values (
+        $2,
+        calculated_unformated_cost,
+        create_shopping_list.id
+      ) returning * into added_product;
+    else
+      -- If it does not, we insert in the dum_public.shopping_lists and the dum_public.shopping_list_details tables
+      insert into dum_public.shopping_list (
+        user_id
+      ) values(
+        dum_public.current_user_id()
+      ) returning * into created_shopping_list;
+
+      insert into dum_public.shopping_list_details (
+        quantity,
+        unformated_cost,
+        shopping_list_id
+      ) values (
+        $2,
+        calculated_unformated_cost,
+        create_shopping_list.id
+      ) returning * into added_product;
+    end if;
+
+    return added_product;
+  end;
+$$ language plpgsql volatile;
+
+comment on function dum_public.create_shopping_list(product_id uuid, quantity integer) is
+  E'Creates the shopping list will all the products that the user want to buy.';
