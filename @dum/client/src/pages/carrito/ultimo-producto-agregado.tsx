b9e01989c0eb2603d239f6ff961639c03f05ec02
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-01 13:31:23 -0600

    Removed all the code related to the Relay Mutation

--- @dum/client/src/pages/carrito/ultimo-producto-agregado.tsx
+++ @dum/client/src/pages/carrito/ultimo-producto-agregado.tsx
@@ -1,26 +1,20 @@
 import { ScrollView } from "native-base";
 import { useRouter } from "next/router";
 import * as React from "react";
-import { useMutation, usePreloadedQuery } from "react-relay";
+import { usePreloadedQuery } from "react-relay";
 import type { RelayProps } from "relay-nextjs";
 import { withRelay } from "relay-nextjs";
 
 import Layout from "../../components/Layout";
 import Loading from "../../components/Loading";
 import MainDepartmentsList from "../../components/MainDepartmentsList";
-import type { CreateShoppingListMutation as CreateShoppingListMutationTypes } from "../../graphql/Mutations/__generated__/CreateShoppingListMutation.graphql";
-import CreateShoppingListMutation from "../../graphql/Mutations/CreateShoppingListMutation";
 import type { AddedProductPageQuery as AddedProductPageQueryTypes } from "../../graphql/Queries/__generated__/AddedProductPageQuery.graphql";
 import AddedProductPageQuery from "../../graphql/Queries/AddedProductPageQuery";
 import { getClientEnvironment } from "../../lib/client";
 
-function AddedProductPage({
+function LastAddedProductPage({
   preloadedQuery,
 }: RelayProps<{}, AddedProductPageQueryTypes>) {
-  const [createShoppingList] = useMutation<CreateShoppingListMutationTypes>(
-    CreateShoppingListMutation
-  );
-
   const router = useRouter();
   React.useEffect(() => {
     const { product_id, quantity } = router.query;
@@ -45,7 +39,7 @@ function AddedProductPage({
   );
 }
 
-export default withRelay(AddedProductPage, AddedProductPageQuery, {
+export default withRelay(LastAddedProductPage, AddedProductPageQuery, {
   createClientEnvironment: () => getClientEnvironment(),
   createServerEnvironment: async (_ctx, { cookies }) => {
     const { createServerEnvironment } = await import("../../lib/server/server");
