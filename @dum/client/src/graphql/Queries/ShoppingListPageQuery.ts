96b0af5712bbe5351ae1c39ec81dd5cb99452089
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-02 15:47:12 -0600

    Added the currentUser{} field and the fragments needed to render the shopping cart list

--- @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
+++ @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
@@ -3,6 +3,12 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query ShoppingListPageQuery {
     ...CurrentUserFragment_user
+    currentUser {
+      id
+      rowId
+      fullName
+    }
     ...MainDepartmentsFragment_mainDepartments
+    ...ProductsInTheShoppingListFragment_productsInTheShoppingList
   }
 `;
