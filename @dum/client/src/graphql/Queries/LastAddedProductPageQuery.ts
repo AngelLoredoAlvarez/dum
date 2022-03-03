3515429b7bac99f81c68b6137141801bb5335b7e
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-03 13:58:57 -0600

    Added the currentUser{} field to know in the LastAddedProductPage.tsx Page if the user is logged in or not

--- @dum/client/src/graphql/Queries/LastAddedProductPageQuery.ts
+++ @dum/client/src/graphql/Queries/LastAddedProductPageQuery.ts
@@ -3,6 +3,14 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   query LastAddedProductPageQuery {
     ...CurrentUserFragment_user
+    currentUser {
+      id
+      rowId
+      fullName
+    }
     ...MainDepartmentsFragment_mainDepartments
+    lastAddedProductInTheShoppingList {
+      ...LastAddedProductInTheShoppingListFragment_lastAddedProduct
+    }
   }
 `;
