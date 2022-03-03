7624069c66ec329c5cf838285318b5e35ffc0356
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-03 13:58:08 -0600

    Added the LastAddedProductInTheShoppingList.ts Fragment

---
+++ @dum/client/src/graphql/Fragments/LastAddedProductInTheShoppingListFragment.ts
@@ -0,0 +1,22 @@
+import { graphql } from "react-relay/hooks";
+
+export default graphql`
+  fragment LastAddedProductInTheShoppingListFragment_lastAddedProduct on ShoppingListDetail {
+    id
+    rowId
+    product {
+      description
+      productPictures {
+        edges {
+          node {
+            id
+            rowId
+            pictureUrl
+          }
+        }
+      }
+    }
+    quantity
+    cost
+  }
+`;
