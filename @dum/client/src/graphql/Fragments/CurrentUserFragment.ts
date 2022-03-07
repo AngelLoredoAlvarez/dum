e3376a0896a58a8314ae38677b9c4807a0602acc
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-07 13:21:43 -0600

    Removed the __id field from the fragment

--- @dum/client/src/graphql/Fragments/CurrentUserFragment.ts
+++ @dum/client/src/graphql/Fragments/CurrentUserFragment.ts
@@ -3,7 +3,6 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   fragment CurrentUserFragment_user on Query {
     currentUser {
-      __id
       id
       rowId
       avatarUrl
