186de9d2fa8a7de5f1f24678c3a646ec8b923577
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-07 13:04:01 -0600

    Added the __id field that allows to query the ID from the CurrentUserFragment.ts Fragment to change values from the Store

--- @dum/client/src/graphql/Fragments/CurrentUserFragment.ts
+++ @dum/client/src/graphql/Fragments/CurrentUserFragment.ts
@@ -3,6 +3,7 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   fragment CurrentUserFragment_user on Query {
     currentUser {
+      __id
       id
       rowId
       avatarUrl
