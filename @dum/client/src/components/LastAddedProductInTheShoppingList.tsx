3291ae8d150f5a0a7b6466bcb55844306ee1eb2d
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-09 09:54:19 -0600

    Added the code to handle errors when the users don't have any items in their shopping cart, and they navigates to the LastAddedProduct.tsx page

--- @dum/client/src/components/LastAddedProductInTheShoppingList.tsx
+++ @dum/client/src/components/LastAddedProductInTheShoppingList.tsx
@@ -79,117 +79,137 @@ function LastAddedProductInTheShoppingList(
       rounded="lg"
       shadow="1"
     >
-      <Box>
-        <Image
-          alt="image"
-          h={{
-            base: "40",
-            sm: "40",
-            md: "215px",
-            lg: "215px",
-            xl: "215px",
-            "2xl": "215px",
-          }}
-          w={{
-            base: "100%",
-            sm: "100%",
-            md: "250px",
-            lg: "250px",
-            xl: "250px",
-            "2xl": "250px",
-          }}
-          source={{
-            uri: `${lastAddedProductInTheShoppingList.product.productPictures.edges[0].node.pictureUrl}`,
-          }}
-        />
-      </Box>
-      <Stack
-        alignContent={"center"}
-        alignItems={"center"}
-        direction={{
-          base: "column",
-          sm: "column",
-          md: "row",
-          lg: "row",
-          xl: "row",
-          "2xl": "row",
-        }}
-        flex={1}
-        p="4"
-        space={[3, 3, 1.5]}
-      >
-        <VStack alignContent={"center"} alignItems={"center"} space={2}>
-          <Heading
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "sm",
-              lg: "sm",
-              xl: "sm",
-              "2xl": "md",
+      {lastAddedProductInTheShoppingList !== null ? (
+        <>
+          <Box>
+            <Image
+              alt="image"
+              h={{
+                base: "40",
+                sm: "40",
+                md: "215px",
+                lg: "215px",
+                xl: "215px",
+                "2xl": "215px",
+              }}
+              w={{
+                base: "100%",
+                sm: "100%",
+                md: "250px",
+                lg: "250px",
+                xl: "250px",
+                "2xl": "250px",
+              }}
+              source={{
+                uri: `${lastAddedProductInTheShoppingList.product.productPictures.edges[0].node.pictureUrl}`,
+              }}
+            />
+          </Box>
+          <Stack
+            alignContent={"center"}
+            alignItems={"center"}
+            direction={{
+              base: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+              "2xl": "row",
             }}
-            textAlign={"center"}
+            flex={1}
+            p="4"
+            space={[3, 3, 1.5]}
           >
-            El Ultimo Producto que agregaste a tu Carrito fue:
-          </Heading>
-          <Text textAlign={"center"}>
-            {lastAddedProductInTheShoppingList.product.description}
-          </Text>
-          <HStack>
-            <Text bold textAlign={"center"}>
-              Cantidad:{" "}
-            </Text>
-            <Text>{lastAddedProductInTheShoppingList.quantity}</Text>
-          </HStack>
-          <HStack>
-            <Text bold textAlign={"center"}>
-              Costo:{" "}
-            </Text>
-            <Text>{lastAddedProductInTheShoppingList.cost}</Text>
-          </HStack>
-        </VStack>
-        {screenSize === "base" || screenSize === "sm" ? (
-          <Divider my={5} orientation={"horizontal"} />
-        ) : (
-          <Divider my={5} orientation={"vertical"} />
-        )}
-        <VStack space={3}>
-          <Heading
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "sm",
-              lg: "sm",
-              xl: "sm",
-              "2xl": "md",
-            }}
-            textAlign={"center"}
-          >
-            ¡Alcanza el Envío Gratis comprando más productos!
-          </Heading>
-          <Text textAlign={"center"}>PROGRESS BAR</Text>
-        </VStack>
-        {screenSize === "base" || screenSize === "sm" ? (
-          <Divider my={5} orientation={"horizontal"} />
-        ) : (
-          <Divider my={5} orientation={"vertical"} />
-        )}
-        <Stack
-          alignContent={"center"}
-          direction={{
-            base: "row",
-            sm: "row",
-            md: "row",
-            lg: "column",
-            xl: "column",
-            "2xl": "column",
+            <VStack alignContent={"center"} alignItems={"center"} space={2}>
+              <Heading
+                size={{
+                  base: "sm",
+                  sm: "sm",
+                  md: "sm",
+                  lg: "sm",
+                  xl: "sm",
+                  "2xl": "md",
+                }}
+                textAlign={"center"}
+              >
+                El Ultimo Producto que agregaste a tu Carrito fue:
+              </Heading>
+              <Text textAlign={"center"}>
+                {lastAddedProductInTheShoppingList.product.description}
+              </Text>
+              <HStack>
+                <Text bold textAlign={"center"}>
+                  Cantidad:{" "}
+                </Text>
+                <Text>{lastAddedProductInTheShoppingList.quantity}</Text>
+              </HStack>
+              <HStack>
+                <Text bold textAlign={"center"}>
+                  Costo:{" "}
+                </Text>
+                <Text>{lastAddedProductInTheShoppingList.cost}</Text>
+              </HStack>
+            </VStack>
+            {screenSize === "base" || screenSize === "sm" ? (
+              <Divider my={5} orientation={"horizontal"} />
+            ) : (
+              <Divider my={5} orientation={"vertical"} />
+            )}
+            <VStack space={3}>
+              <Heading
+                size={{
+                  base: "sm",
+                  sm: "sm",
+                  md: "sm",
+                  lg: "sm",
+                  xl: "sm",
+                  "2xl": "md",
+                }}
+                textAlign={"center"}
+              >
+                ¡Alcanza el Envío Gratis comprando más productos!
+              </Heading>
+              <Text textAlign={"center"}>PROGRESS BAR</Text>
+            </VStack>
+            {screenSize === "base" || screenSize === "sm" ? (
+              <Divider my={5} orientation={"horizontal"} />
+            ) : (
+              <Divider my={5} orientation={"vertical"} />
+            )}
+            <Stack
+              alignContent={"center"}
+              direction={{
+                base: "row",
+                sm: "row",
+                md: "row",
+                lg: "column",
+                xl: "column",
+                "2xl": "column",
+              }}
+              space={1}
+            >
+              <Button onPress={handleRouting}>Ver Carrito</Button>
+              <Button>Comprar Carrito</Button>
+            </Stack>
+          </Stack>
+        </>
+      ) : (
+        <Text
+          flex={1}
+          fontSize={{
+            base: "lg",
+            sm: "lg",
+            md: "xl",
+            lg: "2xl",
+            xl: "3xl",
+            "2xl": "4xl",
           }}
-          space={1}
+          textAlign={"center"}
         >
-          <Button onPress={handleRouting}>Ver Carrito</Button>
-          <Button>Comprar Carrito</Button>
-        </Stack>
-      </Stack>
+          Actualmente, no cuentas con ningun producto en tu carrito 😪 pero...
+          😏
+        </Text>
+      )}
     </Stack>
   );
 }
