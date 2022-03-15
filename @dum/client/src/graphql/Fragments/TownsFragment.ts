88a4d8c0f5e807c67f70514ffc9978269fe1cfa0
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-15 14:59:45 -0600

    Added the TownsFragment.ts Fragment to Query all towns in the DB

---
+++ @dum/client/src/graphql/Fragments/TownsFragment.ts
@@ -0,0 +1,18 @@
+import { graphql } from "react-relay/hooks";
+
+export default graphql`
+  fragment TownsFragment_towns on Query
+  @argumentDefinitions(first: { type: "Int" }, after: { type: "Cursor" })
+  @refetchable(queryName: "Towns") {
+    towns(first: $first, after: $after)
+      @connection(key: "TownsFragment_towns") {
+      edges {
+        node {
+          id
+          rowId
+          name
+        }
+      }
+    }
+  }
+`;
