33430d09d99f17339cc0b20d554a8b87b58877af
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-17 14:35:02 -0600

    Added the search variables to the SuburbsFragment.ts Fragment

--- @dum/client/src/graphql/Fragments/SuburbsFragment.ts
+++ @dum/client/src/graphql/Fragments/SuburbsFragment.ts
@@ -3,8 +3,8 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   fragment SuburbsFragment_suburbs on Query
   @refetchable(queryName: "SuburbsRefetchableQuery")
-  @argumentDefinitions(townId: { type: "UUID" }) {
-    suburbsByTownId(townId: $townId) {
+  @argumentDefinitions(townId: { type: "UUID" }, search: { type: "String" }) {
+    suburbsBySearch(townId: $townId, search: $search) {
       edges {
         node {
           id
