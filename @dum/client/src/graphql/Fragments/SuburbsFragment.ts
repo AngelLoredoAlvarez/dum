f4b0c7be0805d105517a7a06d4b3f89e24626b27
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-29 13:22:45 -0600

    Removed the search variable

--- @dum/client/src/graphql/Fragments/SuburbsFragment.ts
+++ @dum/client/src/graphql/Fragments/SuburbsFragment.ts
@@ -3,8 +3,8 @@ import { graphql } from "react-relay/hooks";
 export default graphql`
   fragment SuburbsFragment_suburbs on Query
   @refetchable(queryName: "SuburbsRefetchableQuery")
-  @argumentDefinitions(townId: { type: "UUID" }, search: { type: "String" }) {
-    suburbsByTownId(townId: $townId, search: $search) {
+  @argumentDefinitions(townId: { type: "UUID" }) {
+    suburbsByTownId(townId: $townId) {
       edges {
         node {
           id
