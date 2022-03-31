6f59c56ab7fd84cb5f16dfc423fc55761805f4e6
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-03-31 07:54:49 -0600

    Added the migration that creates the dum_public.user_addresses

---
+++ @dum/db/migrations/current/1050-user_addresses.sql
@@ -0,0 +1,52 @@
+/*
+ * Table that will store all the Addresses from a User
+ */
+create table dum_public.user_addresses (
+  id uuid primary key default gen_random_uuid(),
+  town_id uuid not null references dum_public.towns(id),
+  suburb_id uuid not null references dum_public.suburbs(id),
+  street_id uuid not null references dum_public.streets(id),
+  ext_number text,
+  int_number text,
+  is_main boolean default false,
+  user_id uuid not null references dum_public.users(id),
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+
+-- Enable ROW lEVEL SECURITY
+alter table dum_public.user_addresses enable row level security;
+
+-- Grant the SELECT permission to all the columns in the table
+grant select on dum_public.user_addresses to :DATABASE_VISITOR;
+
+-- Allow efficient retrieval of all the Towns, Suburbs, Streets owned by a particular User.
+create index idx_user_addresses_town on dum_public.user_addresses(town_id);
+create index idx_user_addresses_suburb on dum_public.user_addresses(suburb_id);
+create index idx_user_addresses_street on dum_public.user_addresses(street_id);
+create index idx_user_addresses_user on dum_public.user_addresses(user_id);
+
+-- Users may only manage their own Addresses.
+create policy select_own on dum_public.user_addresses for select using (user_id = dum_public.current_user_id());
+create policy insert_own on dum_public.user_addresses for insert with check (user_id = dum_public.current_user_id());
+
+comment on table dum_public.user_addresses is
+  E'An address own by a User.';
+
+comment on column dum_public.user_addresses.id is
+  E'Unique identifier for the Address.';
+comment on column dum_public.user_addresses.is_main is
+  E'The Main Address of the User';
+
+create trigger _100_timestamps
+  before insert or update on dum_public.user_addresses
+  for each row
+  execute procedure dum_private.tg__timestamps();
+
+/*
+ * Function that returns all the Addresses from the User
+ */
+ create or replace function dum_public.user_addresses() returns setof dum_public.user_addresses as $$
+  select * from dum_public.user_addresses where user_id = dum_public.current_user_id();
+ $$ language sql stable;
+
