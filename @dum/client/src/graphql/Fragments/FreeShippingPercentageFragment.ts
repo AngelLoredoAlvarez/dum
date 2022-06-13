9c13b28ce842f265a5a8b89b082dc10f62ee2861
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-06-13 15:09:18 -0500

    Added the FreeShippingPercentageFragment.ts Fragment

---
+++ @dum/client/src/graphql/Fragments/FreeShippingPercentageFragment.ts
@@ -0,0 +1,9 @@
+import graphql from "babel-plugin-relay/macro";
+
+export default graphql`
+  fragment FreeShippingPercentageFragment_freeShippingPercentage on Query {
+    currentUserOpenedShoppingList {
+      percentageFreeShipping
+    }
+  }
+`;
