c8806d67a7e00047fb88a87f6bf4b7d9581b74a0
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-06-20 13:29:36 -0500

    Removed the currentUserOpenedShoppingList type from the Fragment, this because of the changes in the ShoppingListPage.tsx Page

--- @dum/client/src/graphql/Fragments/ProductsInTheShoppingListFooterFragment.ts
+++ @dum/client/src/graphql/Fragments/ProductsInTheShoppingListFooterFragment.ts
@@ -1,12 +1,8 @@
 import graphql from "babel-plugin-relay/macro";
 
 export default graphql`
-  fragment ProductsInTheShoppingListFooterFragment_costs on Query {
-    currentUserOpenedShoppingList {
-      id
-      rowId
-      amountToReachFreeShipping
-      totalToPay
-    }
+  fragment ProductsInTheShoppingListFooterFragment_costs on ShoppingList {
+    amountToReachFreeShipping
+    totalToPay
   }
 `;
