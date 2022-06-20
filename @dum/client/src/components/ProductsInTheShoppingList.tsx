e178e9c9f549486ea3200fa2f8fb52a8646eaa2d
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-06-20 11:58:54 -0500

    Removed all the components and Relay Keys except the ones related to the list, we are fixing this page

--- @dum/client/src/components/ProductsInTheShoppingList.tsx
+++ @dum/client/src/components/ProductsInTheShoppingList.tsx
@@ -2,19 +2,14 @@ import { FlatList } from "native-base";
 import * as React from "react";
 import { usePaginationFragment } from "react-relay/hooks";
 
-import type { FreeShippingPercentageFragment_freeShippingPercentage$key } from "../graphql/Fragments/__generated__/FreeShippingPercentageFragment_freeShippingPercentage.graphql";
-import type { MainDepartmentsFragment_mainDepartments$key } from "../graphql/Fragments/__generated__/MainDepartmentsFragment_mainDepartments.graphql";
-import type { ProductsInTheShoppingListFragment_productsInTheShoppingList$key } from "../graphql/Fragments/__generated__/ProductsInTheShoppingListFragment_productsInTheShoppingList.graphql";
+import type { ProductsInTheShoppingListFragment_shoppingListDetails$key } from "../graphql/Fragments/__generated__/ProductsInTheShoppingListFragment_shoppingListDetails.graphql";
 import ProductsInTheShoppingListFragment from "../graphql/Fragments/ProductsInTheShoppingListFragment";
 import Loading from "./Loading";
 import ProductInShoppingListItem from "./ProductInShoppingListItem";
-import ProductsInTheShoppingListHeader from "./ProductsInTheShoppingListHeader";
 
 interface ProductsInTheShoppingListProps {
   currentUserID: string;
-  freeShippingPercentage: FreeShippingPercentageFragment_freeShippingPercentage$key;
-  mainDepartments: MainDepartmentsFragment_mainDepartments$key;
-  productsInTheShoppingList: ProductsInTheShoppingListFragment_productsInTheShoppingList$key;
+  productsInTheShoppingList: ProductsInTheShoppingListFragment_shoppingListDetails$key;
 }
 
 function ProductsInTheShoppingList(props: ProductsInTheShoppingListProps) {
@@ -25,7 +20,7 @@ function ProductsInTheShoppingList(props: ProductsInTheShoppingListProps) {
 
   return (
     <FlatList
-      data={data.productsInTheShoppingList.edges}
+      data={data.shoppingListDetails.edges}
       flex={1}
       keyExtractor={(item) => item.node.id}
       ListHeaderComponentStyle={{
@@ -33,12 +28,6 @@ function ProductsInTheShoppingList(props: ProductsInTheShoppingListProps) {
         width: "100%",
       }}
       ListFooterComponent={hasNext ? isLoadingNext ? <Loading /> : null : null}
-      ListHeaderComponent={
-        <ProductsInTheShoppingListHeader
-          freeShippingPercentage={props.freeShippingPercentage}
-          mainDepartments={props.mainDepartments}
-        />
-      }
       onEndReached={() => {
         loadNext(3);
       }}
@@ -47,7 +36,7 @@ function ProductsInTheShoppingList(props: ProductsInTheShoppingListProps) {
         <ProductInShoppingListItem
           currentUserID={props.currentUserID}
           productInShoppingList={item.node}
-          productsInTheShoppingListID={data.productsInTheShoppingList.__id}
+          productsInTheShoppingListID={data.shoppingListDetails.__id}
         />
       )}
       w={"100%"}
