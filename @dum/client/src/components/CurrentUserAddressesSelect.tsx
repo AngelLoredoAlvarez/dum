5a1d9d9c2f058c687169da5a68fcfa4c8f888c18
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-06-22 08:03:01 -0500

    Created a specific component for the <Select /> component that will display the addresses from the user

---
+++ @dum/client/src/components/CurrentUserAddressesSelect.tsx
@@ -0,0 +1,45 @@
+import { Select } from "native-base";
+import * as React from "react";
+import { useFragment } from "react-relay/hooks";
+
+import type { CurrentUserAddressesFragment_addresses$key } from "../graphql/Fragments/__generated__/CurrentUserAddressesFragment_addresses.graphql";
+import CurrentUserAddressesFragment from "../graphql/Fragments/CurrentUserAddressesFragment";
+
+interface CurrentUserAddressesSelectProps {
+  addresses: CurrentUserAddressesFragment_addresses$key;
+}
+
+function UserAddressesSelect(props: CurrentUserAddressesSelectProps) {
+  const { userAddresses } =
+    useFragment<CurrentUserAddressesFragment_addresses$key>(
+      CurrentUserAddressesFragment,
+      props.addresses
+    );
+
+  return (
+    <Select
+      _text={{
+        color: "red.600",
+      }}
+      fontSize={{
+        base: "sm",
+        sm: "md",
+        md: "lg",
+        lg: "xl",
+        xl: "2xl",
+        "2xl": "2xl",
+      }}
+      placeholder={"Elige la dirección a donde enviaremos tu pedido"}
+    >
+      {userAddresses.edges.map(({ node }) => (
+        <Select.Item
+          key={node.id}
+          label={`COL. ${node.suburb.name}, ${node.town.name}`}
+          value={node.rowId}
+        />
+      ))}
+    </Select>
+  );
+}
+
+export default UserAddressesSelect;
