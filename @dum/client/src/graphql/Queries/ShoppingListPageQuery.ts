ebfdd8f52e53cadf0f9d9f38ed6a2f111f0e117c
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-06-22 08:05:27 -0500

    Spreaded the CurrentUserAddressesFragment.ts Fragment in the Query

--- @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
+++ @dum/client/src/graphql/Queries/ShoppingListPageQuery.ts
@@ -7,6 +7,7 @@ export default graphql`
       id
       rowId
       fullName
+      ...CurrentUserAddressesFragment_addresses
     }
     ...MainDepartmentsFragment_mainDepartments
     currentUserOpenedShoppingList {
