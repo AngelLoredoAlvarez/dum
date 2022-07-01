20afa383e0e0d6a7f3c227afd936a44bcc1ed68f
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-07-01 14:42:48 -0500

    Added the 404.tsx page as a requirement from Next Js

---
+++ @dum/client/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "native-base";
+import * as React from "react";
+
+function Custom404Page() {
+  return (
+    <Box>
+      <Text>Custom 404 Page</Text>
+    </Box>
+  );
+}
+
+export default Custom404Page;
