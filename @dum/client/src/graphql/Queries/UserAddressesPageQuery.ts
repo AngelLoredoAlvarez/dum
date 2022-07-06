8a4402f13cf2825e1138d7f3370c668a8a887a1c
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-07-06 15:40:27 -0500

    Added the UserAddressesQuery.ts Query

---
+++ @dum/client/src/graphql/Queries/UserAddressesPageQuery.ts
@@ -0,0 +1,12 @@
+import graphql from "babel-plugin-relay/macro";
+
+export default graphql`
+  query UserAddressesPageQuery {
+    ...CurrentUserFragment_user
+    currentUser {
+      id
+      rowId
+      fullName
+    }
+  }
+`;
