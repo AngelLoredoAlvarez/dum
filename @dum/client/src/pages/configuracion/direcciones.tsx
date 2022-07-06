e373299a3c135a6ae2341080d956e483823fb2e6
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-07-06 15:41:18 -0500

    Added the UserAddressesPage.tsx Page to allow to the user to manage all the addresses related to him

---
+++ @dum/client/src/pages/configuracion/direcciones.tsx
@@ -0,0 +1,50 @@
+import { Box, Text } from "native-base";
+import * as React from "react";
+import { usePreloadedQuery } from "react-relay";
+import type { RelayProps } from "relay-nextjs";
+import { withRelay } from "relay-nextjs";
+
+import Layout from "../../components/Layout";
+import Loading from "../../components/Loading";
+import Redirect from "../../components/Redirect";
+import type { UserAddressesPageQuery as UserAddressesPageQueryTypes } from "../../graphql/Queries/__generated__/UserAddressesPageQuery.graphql";
+import UserAddressesPageQuery from "../../graphql/Queries/UserAddressesPageQuery";
+import { getClientEnvironment } from "../../lib/client";
+
+function UserAddressesPage({
+  preloadedQuery,
+}: RelayProps<{}, UserAddressesPageQueryTypes>) {
+  const userAddressesPageQuery = usePreloadedQuery<UserAddressesPageQueryTypes>(
+    UserAddressesPageQuery,
+    preloadedQuery
+  );
+
+  if (
+    userAddressesPageQuery.currentUser === null ||
+    userAddressesPageQuery.currentUser === undefined
+  )
+    return <Redirect href="/" />;
+
+  return (
+    <Layout currentUser={userAddressesPageQuery}>
+      <Box>
+        <Text>UserAddressesPage</Text>
+      </Box>
+    </Layout>
+  );
+}
+
+export default withRelay(UserAddressesPage, UserAddressesPageQuery, {
+  createClientEnvironment: () => getClientEnvironment(),
+  createServerEnvironment: async (_ctx, { cookies }) => {
+    const { createServerEnvironment } = await import("../../lib/server/server");
+
+    return createServerEnvironment(cookies);
+  },
+  fallback: <Loading />,
+  serverSideProps: async (ctx) => {
+    const cookies = ctx.req.headers.cookie;
+
+    return { cookies };
+  },
+});
