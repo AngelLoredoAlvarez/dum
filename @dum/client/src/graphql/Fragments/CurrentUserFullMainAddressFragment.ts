f41d9489b369fcf7c4a55a4826d98b8b583b954b
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-07-07 15:40:23 -0500

    Added the CurrentUserFullMainAddress.ts Fragment with the fullMainAddress field

--- @dum/client/src/graphql/Fragments/CurrentUserFullMainAddressFragment.ts
+++ @dum/client/src/graphql/Fragments/CurrentUserFullMainAddressFragment.ts
@@ -2,6 +2,6 @@ import graphql from "babel-plugin-relay";
 
 export default graphql`
   fragment CurrentUserFullMainAddressFragment_currentUserFullMainAddress on User {
-    fullAddress
+    fullMainAddress
   }
 `;
