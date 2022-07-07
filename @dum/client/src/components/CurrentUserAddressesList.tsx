bf77814039ad268eca29fe5a469135589fb98905
Ángel Loredo <angel.loredo.alvarez@gmail.com>
2022-07-07 15:49:00 -0500

    Added the CurrentUserFullMainAddress key reference

--- @dum/client/src/components/CurrentUserAddressesList.tsx
+++ @dum/client/src/components/CurrentUserAddressesList.tsx
@@ -3,10 +3,13 @@ import * as React from "react";
 import { usePaginationFragment } from "react-relay/hooks";
 
 import type { CurrentUserAddressesListFragment_currentUserAddresses$key } from "../graphql/Fragments/__generated__/CurrentUserAddressesListFragment_currentUserAddresses.graphql";
+import type { CurrentUserFullMainAddressFragment_currentUserFullMainAddress$key } from "../graphql/Fragments/__generated__/CurrentUserFullMainAddressFragment_currentUserFullMainAddress.graphql";
 import UserAddressesListFragment from "../graphql/Fragments/CurrentUserAddressesListFragment";
 import CurrentUserAddressesListHeader from "./CurrentUserAddressesListHeader";
+
 interface UserAddressesListProps {
   userAddresses: CurrentUserAddressesListFragment_currentUserAddresses$key;
+  currentUserFullMainAddress: CurrentUserFullMainAddressFragment_currentUserFullMainAddress$key;
 }
 
 function UserAddressesList(props: UserAddressesListProps) {
@@ -17,7 +20,11 @@ function UserAddressesList(props: UserAddressesListProps) {
   return (
     <FlatList
       data={data.userAddresses.edges}
-      ListHeaderComponent={<CurrentUserAddressesListHeader />}
+      ListHeaderComponent={
+        <CurrentUserAddressesListHeader
+          fullMainAddress={props.currentUserFullMainAddress}
+        />
+      }
       keyExtractor={(item) => item.node.id}
       renderItem={({ item }) => <Text>{item.node.id}</Text>}
     />
